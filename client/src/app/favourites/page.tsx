a85033f304b3dfff54aeab904d8819ebbad4cc0d
khuzaimashoaib <khuzaima.shoaib@gmail.com>
2026-03-26 10:17:00 +0500

    fix pages

---
+++ client/src/app/favourites/page.tsx
@@ -0,0 +1,7 @@
+export default function FavouritesPage() {
+  return (
+    <div>
+      <h1>Favourites Coming Soon</h1>
+    </div>
+  );
+}
---
+++ client/src/app/properties/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function PropertyDetailPage() {
+  return (
+    <div>
+      <h1>Property Detail Coming Soon</h1>
+    </div>
+  );
+}
