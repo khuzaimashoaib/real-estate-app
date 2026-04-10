ac8a32f1362da29f444c636dc10cf55d1c082b76
khuzaimashoaib <khuzaima.shoaib@gmail.com>
2026-04-10 09:31:31 +0500

    AuthController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -11,8 +11,8 @@ const generateToken = (id) => {
 const setTokenCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
